47b8bdc790081b249b9d3b9d381d7ec56a464b46
vinwen91 <83156741+vinwen91@users.noreply.github.com>
2021-09-28 20:21:54 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,14 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+     An NFT is a unit of data stored on a digital ledger, called a blockchain, which can be sold and traded. 
+     The NFT can be associated with a particular digital or physical asset (such as a file or a physical object) and a license to use the asset for a specified purpose.
+     NFTs (and the associated license to use, copy or display the underlying asset) can be traded and sold on digital markets.
+     NFTs function like cryptographic tokens, but, unlike cryptocurrencies like Bitcoin, NFTs are not mutually interchangeable, hence not fungible. 
+     While all bitcoins are equal, each NFT may represent a different underlying asset and thus have a different value.
+     NFTs are created when blockchains string records of cryptographic hash, a set of characters identifying a set of data, onto previous records therefore creating a chain of identifiable data blocks.
+     This cryptographic transaction process ensures the authentication of each digital file by providing a digital signature that is used to track NFT ownership.
+     However, data links that point to details like where the art is stored can die.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
